e8430945031d633dd3cd61447474a47eb7d381da
fattyray <96484926+fattyray@users.noreply.github.com>
2022-10-30 21:29:30 +0800

    update

--- change_pic.js
+++ change_pic.js
@@ -1,10 +1,10 @@
 select=document.getElementById("picshow");
 count=0
-total=3
+total=8
 changepic=function () {
     name='img_'+(count%total+1)+'.png';
     select.style.backgroundImage="url("+name+")";
     count+=1
 }
 changepic()
-timer=setInterval(changepic,4000)
+timer=setInterval(changepic,2500)
